d0a857b75d53516e823d287b7ef43bfe21113013
beaviz <43408084+beaviz@users.noreply.github.com>
2025-02-26 15:05:20 +0000

    fix error 3

--- main.tf
+++ main.tf
@@ -38,9 +38,9 @@ resource "aws_instance" "blog" {
 
   resource "aws_security_group_rule" "blog_http_in" {
     type = "ingress"
-    from_Port = 80
+    from_port = 80
     to_port = 80
-    Port = "tcp"
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
     security_group_id = aws_security_group.blog.id
@@ -49,18 +49,19 @@ resource "aws_instance" "blog" {
 
   resource "aws_security_group_rule" "blog_https_in" {
     type = "ingress"
-    from_Port = 443
+    from_port = 443
     to_port = 443
-    Port = "tcp"
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
     security_group_id = aws_security_group.blog.id
+  }
 
     resource "aws_security_group_rule" "blog_everything_out" {
     type = "egress"
-    from_Port = 0
+    from_port = 0
     to_port = 0
-    Port = "-1"
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
 
     security_group_id = aws_security_group.blog.id
